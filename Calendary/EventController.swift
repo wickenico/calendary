957e104f66120d8e06866355d330df27bc0d06d0
Nico Wickersheim <nico.wickersheim@yahoo.de>
2019-03-19 22:47:00 +0100

    initial commit

---
+++ Calendary/EventController.swift
@@ -0,0 +1,43 @@
+//
+//  EventController.swift
+//  Calendary
+//
+//  Created by Nico Wickersheim on 19.03.19.
+//  Copyright © 2019 Nico Wickersheim. All rights reserved.
+//
+
+import UIKit
+
+class EventController: UITableViewController {
+    
+    let fruits = ["Apple", "Banana", "Raspberry"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
+        
+        let fruit = fruits[indexPath.row]
+        
+        cell.textLabel?.text = "Section \(indexPath.section), Row \(indexPath.row): \(fruit)"
+        
+        return cell
+    }
+    
+    
+    
+}
---
+++ Calendary/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Calendary
+//
+//  Created by Nico Wickersheim on 15.03.19.
+//  Copyright © 2019 Nico Wickersheim. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
